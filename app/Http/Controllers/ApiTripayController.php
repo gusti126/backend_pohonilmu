5f943712eb587c1e8f9ac1a9ee09c145c3de01bc
gusti126 <gustirizkia4@gmail.com>
2021-04-29 21:26:57 +0700

    memperbiki tripay payment gatewaye

--- app/Http/Controllers/ApiTripayController.php
+++ app/Http/Controllers/ApiTripayController.php
@@ -86,7 +86,7 @@ class ApiTripayController extends Controller
         // $response = Http::withToken('Bearer'.$apiKey)->post('https://payment.tripay.co.id/api-sandbox/transaction/create', $data);
         $response = Http::withHeaders([
             'Authorization' => 'Bearer '.$apiKey])->post('https://payment.tripay.co.id/api/transaction/create', $data);
-        dd($response['data']);
+        dd($response);
         // return $response;
         $kode_pempayaran = $response['data']['pay_code'];
         $no_refernsi = $response['data']['reference'];
